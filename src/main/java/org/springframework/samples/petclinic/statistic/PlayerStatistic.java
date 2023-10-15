4042fb3895f5b1198e102c3a1731f05ba4553183
alvarobernal2412 <80347035+alvarobernal2412@users.noreply.github.com>
2023-10-15 15:53:44 +0200

    fix: Change joincolum
    The @Colum has been mentioned to have it in case it needs to be used and @JoinColum deleted.

--- src/main/java/org/springframework/samples/petclinic/statistic/PlayerStatistic.java
+++ src/main/java/org/springframework/samples/petclinic/statistic/PlayerStatistic.java
@@ -15,31 +15,31 @@ import lombok.Setter;
 @Entity
 @Table(name = "player_statistics")
 public class PlayerStatistic extends BaseEntity {
-    @Column(name = "number_of_games")
+    // @Column(name = "number_of_games")
     @NotNull
     private Integer number_of_games;
 
-    @JoinColumn(name = "win_number")
+    // @Column(name = "win_number")
     @NotNull
     private Integer win_number;
 
-    @JoinColumn(name = "lose_number")
+    // @Column(name = "lose_number")
     @NotNull
     private Integer lose_number;
 
-    @JoinColumn(name = "competitive_points")
+    // @Column(name = "competitive_points")
     @NotNull
     private Integer competitive_points; 
 
-    @JoinColumn(name = "avg_duration")
+    // @Column(name = "avg_duration")
     @NotNull
     private Integer avg_duration;// Duration in Seconds
 
-    @JoinColumn(name = "max_duration")
+    // @Column(name = "max_duration")
     @NotNull
     private Integer max_duration;// Duration in Seconds
 
-    @JoinColumn(name = "min_duration")
+    // @Column(name = "min_duration")
     @NotNull
     private Integer min_duration;// Duration in Seconds
 }
