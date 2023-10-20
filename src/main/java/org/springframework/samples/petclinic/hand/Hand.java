fcbe7e35c6a8b7cf78e3fc0c20b099416fb5b58c
ORTA <mortaper@gmail.com>
2023-10-20 12:16:23 +0200

    feat: added hand-card Relation

--- src/main/java/org/springframework/samples/petclinic/hand/Hand.java
+++ src/main/java/org/springframework/samples/petclinic/hand/Hand.java
@@ -2,11 +2,19 @@ package org.springframework.samples.petclinic.hand;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
+
+
 import org.springframework.samples.petclinic.model.BaseEntity;
+import org.springframework.samples.petclinic.card.Card;
 
 @Entity
 @Getter
@@ -16,5 +24,11 @@ public class Hand extends BaseEntity {
     @Column(name = "num_cartas")
     @NotBlank
     private Integer numCartas;
+
+    @OneToMany
+    @NotNull
+    @Size(min = 1)
+    private List<Card> cards;
+
     
 }
