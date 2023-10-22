07f8018bfc9be9be399abb8e0093585318e93ac1
LUCAS ANTOÑANZAS <lucantdel@alum.us.es>
2023-10-22 16:33:10 +0200

    feat: added relation between deck and card

--- src/main/java/org/springframework/samples/petclinic/card/Card.java
+++ src/main/java/org/springframework/samples/petclinic/card/Card.java
@@ -2,6 +2,8 @@ package org.springframework.samples.petclinic.card;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -12,13 +14,16 @@ import org.springframework.samples.petclinic.model.BaseEntity;
 @Entity
 @Getter
 @Setter
-@Table(name = "card")
+@Table(name = "cards")
 public class Card extends BaseEntity {
 
     @Column(name = "image")
     @NotBlank
     @NotNull
     private String image;
-
     
+    @ManyToOne
+    @JoinColumn(name = "deck_id")
+    private Deck deck;
+
 }
--- src/main/java/org/springframework/samples/petclinic/card/Deck.java
+++ src/main/java/org/springframework/samples/petclinic/card/Deck.java
@@ -1,9 +1,13 @@
 package org.springframework.samples.petclinic.card;
 
+import java.util.List;
+
 import org.springframework.samples.petclinic.model.BaseEntity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
@@ -12,10 +16,13 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-@Table(name = "deck")
+@Table(name = "decks")
 public class Deck extends BaseEntity {
 
     @Column(name = "number_of_cards")
     @NotNull
     private Integer numberOfCards;
+
+    @OneToMany(mappedBy = "deck", fetch = FetchType.EAGER)
+    private List<Card> cards;
 }
