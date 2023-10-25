d5e937e0a4c1a37fe1debb6f973bfe7f3f8c9d13
Ronald752 <ronmonalb@alum.us.es>
2023-10-25 12:36:21 +0200

    feat: Update of the relationship

--- src/main/java/org/springframework/samples/petclinic/symbol/Symbol.java
+++ src/main/java/org/springframework/samples/petclinic/symbol/Symbol.java
@@ -4,11 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.samples.petclinic.card.Card;
+import java.util.List;
 
 @Entity
 @Setter
@@ -20,5 +23,8 @@ public class Symbol {
     @NotNull
     @Enumerated(EnumType.STRING)
     Name name;
+
+    @ManyToMany(mappedBy = "symbols")
+    private List<Card> cards;
     
 }
