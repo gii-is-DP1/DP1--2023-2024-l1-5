3664731547a3590e4c4f06a2fe4e15e62449d60f
Manuel Orta <manortper1@alum.us.es>
2023-12-09 12:49:04 +0100

    fix: solved Game invitation problems

--- frontend/src/player/waitingRoom.js
+++ frontend/src/player/waitingRoom.js
@@ -1,95 +1,22 @@
-import React,{useState,useEffect,useRef, useInterval } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import '../App.css';
 import "../static/css/player/quickWaitingRoom.css";
 import "../static/css/player/newGame.css"
-import { useParams,Link, } from 'react-router-dom';
+import { useParams, Link, } from 'react-router-dom';
+import { useInterval } from 'react-use';
 import tokenService from '../services/token.service';
+
 
 const user = tokenService.getUser();
 
-export default function WaitingRoom(){
-    const {id} = useParams();    
-    const[game,setGame]=useState({});   
-    const [players,setPlayers]=useState([]);
-    const [playerNames,setPlayerNames]=useState([]);
-    const [FriendsNotPlaying, setFriendsNotPlaying] = useState([]);
+export default function WaitingRoom() {
+    const { id } = useParams();
+    const [game, setGame] = useState({});
+    const [players, setPlayers] = useState([]);
+    const [playerNames, setPlayerNames] = useState([]);
+    const [friendsNotPlaying, setFriendsNotPlaying] = useState([]);
     const [friendUsername, setFriendUsername] = useState('');
     const tableRef = useRef(null);
-    
-
-
-    const getGame = async () => {
-        try {
-            const jwt = JSON.parse(window.localStorage.getItem("jwt"));
-            const response = await fetch(`/api/v1/games/${id}`, {
-                method: 'GET',
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: `Bearer ${jwt}`,
-                },
-            });
-            if (response.ok) {
-                const data = await response.json();
-                setGame(data);
-                setPlayers(data.playerList);
-            } else {
-                console.error("Error al obtener la partida", response.statusText);
-            }
-        } catch (error) {
-            console.error("Error al obtener la partida", error);
-        }
-    };
-
-    const getPlayerNames = async () => {
-        try {
-            const jwt = JSON.parse(window.localStorage.getItem("jwt"));
-            const playersPromises = players.map(async (x) => {
-                const response = await fetch(`/api/v1/players/${x.id}`, {
-                    method: 'GET',
-                    headers: {
-                        "Content-Type": "application/json",
-                        Authorization: `Bearer ${jwt}`,
-                    },
-                });
-                const data = await response.json();
-                return data.playerUsername;
-            });
-            const playerNames = await Promise.all(playersPromises);
-            setPlayerNames(playerNames);
-        } catch (error) {
-            console.error("Error al obtener el nombre del jugador", error);
-        }
-    };
-
-    const getFriendsNotPlaying = async () => {
-        try {
-            const jwt = JSON.parse(window.localStorage.getItem("jwt"));
-            const userIdResponse = await fetch(`/api/v1/players/user/${user.id}`, {
-                headers: {
-                    Authorization: `Bearer ${jwt}`,
-                    "Content-Type": "application/json",
-                },
-            });
-            if (userIdResponse.ok) {
-                const responseBody = await userIdResponse.json();
-                const playerId = responseBody.id;
-
-                const friendsPlayingResponse = await fetch(`/api/v1/friendship/friends/notplaying/${playerId}`, {
-                    headers: {
-                        Authorization: `Bearer ${jwt}`,
-                        "Content-Type": "application/json",
-                    },
-                });
-
-                if (friendsPlayingResponse.ok) {
-                    const friendsNotPlayingList = await friendsPlayingResponse.json();
-                    setFriendsNotPlaying(friendsNotPlayingList);
-                }
-            }
-        } catch (error) {
-            console.error('Error fetching friends not playing:', error);
-        }
-    };
 
     const sendInvitationRequest = async (friendUsername) => {
         try {
@@ -104,7 +31,7 @@ export default function WaitingRoom(){
             });
             if (response.ok) {
                 setFriendUsername(friendUsername);
-                alert("ENVIADA")
+                alert("ENVIADA");
             } else {
                 const errorMessage = await response.text();
                 console.error('Error sending Invitation:', errorMessage);
@@ -116,25 +43,86 @@ export default function WaitingRoom(){
         }
     };
 
+    const fetchData = async () => {
+        try {
+            console.log("fetchData llamado");
+            const jwt = JSON.parse(window.localStorage.getItem("jwt"));
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                await getGame();
-                await getPlayerNames();
-                await getFriendsNotPlaying();
-               // setTimeout(fetchData, 10000);
-            } catch (error) {
-                console.error('Error in fetchData:', error);
+            // Obtener información del juego
+            const gameResponse = await fetch(`/api/v1/games/${id}`, {
+                method: 'GET',
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${jwt}`,
+                },
+            });
+
+            if (gameResponse.ok) {
+                const gameData = await gameResponse.json();
+                setGame(gameData);
+                setPlayers(gameData.playerList);
+
+                // Obtener nombres de los jugadores
+                const playersPromises = gameData.playerList.map(async (x) => {
+                    const playerResponse = await fetch(`/api/v1/players/${x.id}`, {
+                        method: 'GET',
+                        headers: {
+                            "Content-Type": "application/json",
+                            Authorization: `Bearer ${jwt}`,
+                        },
+                    });
+                    const playerData = await playerResponse.json();
+                    return playerData.playerUsername;
+                });
+                const playerNames = await Promise.all(playersPromises);
+                setPlayerNames(playerNames);
+
+                // Obtener amigos que no están jugando
+                const userIdResponse = await fetch(`/api/v1/players/user/${user.id}`, {
+                    headers: {
+                        Authorization: `Bearer ${jwt}`,
+                        "Content-Type": "application/json",
+                    },
+                });
+
+                if (userIdResponse.ok) {
+                    const responseBody = await userIdResponse.json();
+                    const playerId = responseBody.id;
+
+                    const friendsPlayingResponse = await fetch(`/api/v1/friendship/friends/notplaying/${playerId}`, {
+                        headers: {
+                            Authorization: `Bearer ${jwt}`,
+                            "Content-Type": "application/json",
+                        },
+                    });
+
+                    if (friendsPlayingResponse.ok) {
+                        const friendsNotPlayingList = await friendsPlayingResponse.json();
+                        setFriendsNotPlaying(friendsNotPlayingList);
+                    }
+                }
+            } else {
+                console.error("Error al obtener la partida", gameResponse.statusText);
             }
-        };
+        } catch (error) {
+            console.error('Error in fetchData:', error);
+        }
+    };
 
+    console.log("Componente renderizado");
+    useEffect(() => {
         fetchData();
+        console.log("hola")
 
-        return () => clearTimeout();
-    }, [id, players]);
+        const intervalId = setInterval(() => {
+            fetchData();
+        }, 10000);
 
-    const FriendsInviteFloatingBox = ({ friendNotPlaying, sendInvitationRequest }) => {
+        return () => clearInterval(intervalId); // Limpiar el intervalo cuando el componente desmonte
+
+    }, [id]);
+
+    const FriendsInviteFloatingBox = ({ friendNotPlaying }) => {
         return (
             <div className="floating-box" style={{ maxWidth: '800px', position: 'fixed', bottom: '20px', right: '20px', backgroundColor: 'white', padding: '10px', borderRadius: '8px', boxShadow: '0px 0px 10px 0px rgba(0,0,0,0.2)' }}>
                 <h3>Friends to Invite</h3>
@@ -158,74 +146,8 @@ export default function WaitingRoom(){
             </div>
         );
     };
-    const InvitationsFloatingBox = ({ invitations }) => {
-        return (
-            <div className="invitation-box floating-box" style={{ maxWidth: '800px' }}>
-                <h3>INVITATIONS</h3>
-                <ul style={{ listStyleType: 'none', padding: 0 }}>
-                    {invitations.map((invitation) => (
-                        <li key={invitation.id} style={{ marginBottom: '10px' }}>
-                            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
-                                <span style={{ marginRight: '5px' }}>
-                                    <a>{invitation.source_user}</a>
-                                </span>
-                                <span style={{ marginRight: '5px' }}>
-                                    <Link to={`/game/quickPlay/${invitation.game.id}`} className='aceptar-boton' onClick={() => handleButton(invitation.game, invitation.id, 'ACEPTAR')}>ACEPTAR</Link>
-                                </span>
-                                <span>
-                                    <div className='rechazar-boton' onClick={() => handleButton(invitation.game, invitation.id, 'RECHAZAR')}>RECHAZAR</div>
-                                </span>
-                            </div>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        );
-    };
-    async function handleButton(game, invitationId, action) {
-        if (action == "ACEPTAR"){
-            const jwt = JSON.parse(window.localStorage.getItem("jwt"));
-            const invitationResponse = await fetch(`/api/v1/invitations/acceptRequest/${invitationId}`,
-            {
-                method: 'PUT',
-                headers: {
-                    "Content-Type": "application/json", 
-                    Authorization: `Bearer ${jwt}`,
-                },
-            });
-            if(invitationResponse.ok){
-                const jwt = JSON.parse(window.localStorage.getItem("jwt"));
 
-                const gameResponse = await fetch(`/api/v1/games/quick/joinInvitation/${game.id}`,
-                {
-                    method: 'PUT',
-                    headers: {
-                        "Content-Type": "application/json", 
-                        Authorization: `Bearer ${jwt}`,
-                    },
-                });
-                if(gameResponse.ok){
-
-                }
-            }
-        }else{
-            const jwt = JSON.parse(window.localStorage.getItem("jwt"));
-            const invitationResponse = await fetch(`/api/v1/invitations/refusededRequest/${invitationId}`,
-            {
-                method: 'PUT',
-                headers: {
-                    "Content-Type": "application/json", 
-                    Authorization: `Bearer ${jwt}`,
-                },
-            });
-            alert("REFUSED")
-            
-        }
-    }
-    
-    
-
-    return(
+    return (
         <div className="wallpaper">
             <div className="horizontal">
                 <div className='distr'>
@@ -233,24 +155,20 @@ export default function WaitingRoom(){
                     <div className='distrPlay'>
                         <span className='text2'>  Players  {game.numPlayers} / 8 </span>
                         <ul>
-                    {playerNames.map((player,index) => {
-                        return(<li key={index}> {player} </li>)
-                    } )}
-                  </ul>
+                            {playerNames.map((player, index) => {
+                                return (<li key={index}> {player} </li>)
+                            })}
+                        </ul>
                     </div>
-                </div>  
+                </div>
                 <div className='vertical'>
                     <div className="inButton">
                         <Link className='button'>The Pit </Link>
                     </div>
                     <div className="social">
-                        {FriendsNotPlaying.length > 0 && <FriendsInviteFloatingBox friendNotPlaying={FriendsNotPlaying} sendInvitationRequest={sendInvitationRequest} />}
-
+                        {friendsNotPlaying.length > 0 && <FriendsInviteFloatingBox friendNotPlaying={friendsNotPlaying} />}
                     </div>
                 </div>
-
-                
-
             </div>
         </div>
     );
