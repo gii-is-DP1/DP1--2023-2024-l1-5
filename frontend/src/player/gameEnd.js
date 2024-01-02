4d6b2932b391f076d0d714e3c911baa274bd07d2
guillegomezz8 <guigomrom@alum.us.es>
2024-01-02 15:09:09 +0100

    winner and loser view

---
+++ frontend/src/player/gameEnd.js
@@ -0,0 +1,42 @@
+import { useNavigate } from 'react-router-dom';
+import "../static/css/player/gameEnd.css";
+
+export default function GameEnd() {
+    const navigate = useNavigate();
+
+    const goToMainMenu = () => {
+        navigate('/');
+    };
+
+    const startNewGame = () => {
+        navigate('/game');
+    };
+
+    const WinnerEnd = () => (
+        <div className="container">
+            <h1>👑¡Felicidades, has ganado!👑</h1>
+            <h4>¿Qué te gustaría hacer a continuación?</h4>
+            <div className="botones-container">
+                <button className="boton_left_winner" onClick={goToMainMenu}>Volver al Menú Principal</button>
+                <button className="boton_rigth_winner" onClick={startNewGame}>Iniciar Nueva Partida</button>
+            </div>
+        </div>
+    );
+
+    const LosserEnd = () => (
+        <div className="container">
+            <h1>❌¡Lo siento, has perdido!❌</h1>
+            <h4>¿Qué te gustaría hacer a continuación?</h4>
+            <div className="botones-container">
+                <button className="boton_left_losser" onClick={goToMainMenu}>Volver al Menú Principal</button>
+                <button className="boton_rigth_losser" onClick={startNewGame}>Iniciar Nueva Partida</button>
+            </div>
+        </div>
+    );
+
+    return(
+        <div className="game-end-container">
+            <WinnerEnd />
+        </div>
+    );
+}
