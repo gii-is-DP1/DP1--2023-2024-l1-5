f466e1bfc0500f877833e06a30889a91370bccf1
LUCAS ANTOÑANZAS <lucantdel@alum.us.es>
2024-01-11 12:47:00 +0100

    fix: chat controller test simplified

--- src/test/java/org/springframework/samples/petclinic/chat/ChatMessageControllerTest.java
+++ src/test/java/org/springframework/samples/petclinic/chat/ChatMessageControllerTest.java
@@ -1,39 +1,25 @@
 package org.springframework.samples.petclinic.chat;
 
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import java.time.LocalDateTime;
-import java.util.Arrays;
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
-import org.springframework.http.MediaType;
 import org.springframework.samples.petclinic.configuration.SecurityConfiguration;
 import org.springframework.samples.petclinic.game.Game;
 import org.springframework.samples.petclinic.game.GameService;
-import org.springframework.samples.petclinic.player.Player;
-import org.springframework.samples.petclinic.player.PlayerService;
-import org.springframework.samples.petclinic.player.State;
-import org.springframework.samples.petclinic.user.User;
-import org.springframework.samples.petclinic.user.UserService;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.transaction.annotation.Transactional;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @WebMvcTest(controllers = ChatMessageController.class, excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = WebSecurityConfigurer.class), excludeAutoConfiguration = SecurityConfiguration.class)
 class ChatMessageControllerTest {
@@ -41,8 +27,6 @@ class ChatMessageControllerTest {
     private static final String BASE_URL = "/api/v1/chatMessages";
     private static final int TEST_CHAT_MESSAGE_ID = 1;
     private static final int TEST_GAME_ID = 1;
-    private static final int TEST_PLAYER_ID = 1;
-    private static final int TEST_USER_ID = 1;
 
     @MockBean
     private GameService gameService;
@@ -50,34 +34,14 @@ class ChatMessageControllerTest {
     @MockBean
     private ChatMessageService chatMessageService;
 
-    @MockBean
-    private UserService userService;
-
-    @MockBean
-    private PlayerService playerService;
-
     @Autowired
     private MockMvc mockMvc;
 
     private Game game;
-    private User user;
-    private Player player;
     private ChatMessage chatMessage;
 
     @BeforeEach
     void setup() {
-
-        player = new Player();
-        player.setId(TEST_PLAYER_ID);
-        player.setFirstName("Lucas");
-        player.setLastName("Antonanzas");
-        player.setImage("image");
-        player.setState(State.ACTIVE);
-        user = new User();
-        user.setId(TEST_USER_ID);
-        user.setUsername("player");
-        user.setPassword("player");
-        player.setUser(user);
 
         game = new Game();
         game.setId(TEST_GAME_ID);
@@ -85,7 +49,7 @@ class ChatMessageControllerTest {
         chatMessage = new ChatMessage();
         chatMessage.setId(TEST_CHAT_MESSAGE_ID);
         chatMessage.setGame(game);
-        chatMessage.setSource_user(player.getPlayerUsername());
+        chatMessage.setSource_user("player");
         chatMessage.setContent("message");
         chatMessage.setMessage_date(LocalDateTime.now());
     }
