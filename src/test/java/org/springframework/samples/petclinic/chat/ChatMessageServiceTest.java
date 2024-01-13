2ee6600e611d98ca6d9c92eabad507ee1b227ac3
LUCAS ANTOÑANZAS <lucantdel@alum.us.es>
2024-01-13 10:49:41 +0100

    fix: chat service tests

--- src/test/java/org/springframework/samples/petclinic/chat/ChatMessageServiceTest.java
+++ src/test/java/org/springframework/samples/petclinic/chat/ChatMessageServiceTest.java
@@ -4,26 +4,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import java.util.List;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class ChatMessageServiceTest {
 
     private static final Integer TEST_GAME_ID = 1;
-    private ChatMessageService chatMessageService;
 
     @Autowired
-    public ChatMessageServiceTest(ChatMessageService chatMessageService) {
-        this.chatMessageService = chatMessageService;
-    }
+    private ChatMessageService chatMessageService;
 
     @Test
-    void testFindAll() {
+    public void testFindAll() {
         List<ChatMessage> chatMessages = this.chatMessageService.getChatMessages();
 
         assertNotNull(chatMessages);
@@ -31,7 +25,7 @@ public class ChatMessageServiceTest {
     }
 
     @Test
-    void testFindChatMessagesByGameId() {
+    public void testFindChatMessagesByGameId() {
         List<ChatMessage> chatMessages = this.chatMessageService.getChatMessagesByGameId(TEST_GAME_ID);
 
         assertNotNull(chatMessages);
@@ -40,7 +34,7 @@ public class ChatMessageServiceTest {
 
     @Test
     @Transactional
-    void testSaveChatMessage() {
+    public void testSaveChatMessage() {
         ChatMessage newMessage = new ChatMessage();
         newMessage.setContent("Test message");
         newMessage.setSource_user("TestUser");
